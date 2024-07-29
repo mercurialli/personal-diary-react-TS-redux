3f564030e7f5a3e17e591fb03799ba46bad7a959
mercurialli <ninoklala2@gmail.com>
2024-07-29 17:21:33 +0300

    add redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Application from './components/Application';
 import { Provider } from 'react-redux';
-import { store } from './services/store';
+import { store, persister } from './services/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 // Application to Render
 
 const app = (
   <Provider store={store}>
-    <Application />
+    <PersistGate loading={null} persistor={persister}>
+      <Application />
+    </PersistGate>
   </Provider>
 );
 
--- src/services/store.tsx
+++ src/services/store.tsx
@@ -1,15 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import notesReducer from './noteSlice';
 import modalReducer from './modalSlice';
+import {
+  persistStore,
+  persistReducer,
+  PERSIST,
+  PAUSE,
+  REHYDRATE,
+  FLUSH,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  notes: notesReducer,
+  modal: modalReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    notes: notesReducer,
-    modal: modalReducer,
-  },
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persister = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
