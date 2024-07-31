85f1a1ea3672b551ab806625b63f1254cb3b2245
mercurialli <ninoklala2@gmail.com>
2024-07-31 16:23:53 +0300

    fix some styles and add presentations files

---
+++ src/presentations/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+// import React, { FC } from 'react';
+// import { Header } from '../Header/Header';
+// import { Footer } from '../Footer/Footer';
+
+// export const Layout = ({ children }) => {
+//   return (
+//     <div id='container'>
+//       <Header />
+//       <div className='main-container'>{children}</div>
+//       <Footer />
+//     </div>
+//   );
+// };
