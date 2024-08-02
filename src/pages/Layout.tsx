43968cf01b03e952841b474dea03dde0e2263235
mercurialli <ninoklala2@gmail.com>
2024-08-02 17:29:18 +0300

    fix

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,7 @@
 import { Footer } from '@src/presentations/Footer/Footer';
 import { Header } from '@src/presentations/Header/Header';
 import React, { FC } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import styles from './Layout.module.scss';
 import { Sidebar } from '@src/presentations/Sidebar/Sidebar';
 
